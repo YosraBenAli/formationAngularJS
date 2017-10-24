7a3f1c83d24d1a410048bc469a37960ed242ddd5
YosraBenAli <byhtuqgj1>
2017-10-24 17:56:44 +0100

    Formation AngularJS : CRUD with $http service

--- appForm.js
+++ appForm.js
@@ -1,46 +1,50 @@
-var app = angular.module('myApp',['ui.bootstrap.showErrors']);
+var app = angular.module('myApp', ['ui.bootstrap.showErrors']);
 
-app.controller('studentController', function($scope) {
-     $scope.students = [
-        { id : 1, firstName : 'Jack', lastName : 'Peterson', age : 17},
-        { id : 2, firstName : 'Mary', lastName : 'Brown', age : 16},
-        { id : 3, firstName : 'Jane', lastName : 'Doe', age : 19}
+app.controller('studentController', function ($scope) {
+    $scope.students = [
+        {id: 1, firstName: 'Jack', lastName: 'Peterson', age: 17},
+        {id: 2, firstName: 'Mary', lastName: 'Brown', age: 16},
+        {id: 3, firstName: 'Jane', lastName: 'Doe', age: 19}
     ];
-     $scope.student = {}; // initialise $scope , nécessaire pour la fonction editStudent
+    $scope.student = {}; // initialise $scope , nécessaire pour la fonction editStudent
 
     // Ajouter un student
-    $scope.addStudent = function(isValid) {
+    $scope.addStudent = function (valid) {
         $scope.$broadcast('show-errors-check-validity');
-        if (isValid) {
-            if($scope.student.id == null){
+        if (valid) {
+            if ($scope.student.id == null) {
                 $scope.student.id = new Date().getMilliseconds();
                 $scope.students.push($scope.student);
                 $scope.alert = 'Student added.';
                 $scope.$broadcast('show-errors-reset');
-
                 // Vider le formulaire après l'ajout
                 $scope.student = {};
-                //$scope.form1 = {};
+
+                //$scope.form1 = {}; ne peut pas ajouter un autre student sans actualiser la page
 
             } else {
                 var index = _.findIndex($scope.students, {'id': $scope.student.id});
                 angular.copy($scope.student, $scope.students[index]);
                 $scope.alert = "Student's data updated.";
-
-                // vider le formulaire après la mise à jour
+                $scope.$broadcast('show-errors-reset');
+                // Vider le formulaire après la mise à jour
                 $scope.student = {};
+
+                //$scope.form1 = {}; ne peut pas mettre à jour les données d'un autre student sans actualiser la page
             }
         }
     };
 
     // Modifier les données d'un student
-    $scope.editStudent = function(index) {
+    $scope.editStudent = function (index) {
         angular.copy($scope.students[index], $scope.student);
     };
 
     // Supprimer un student
-    $scope.deleteStudent = function(index) {
-        $scope.students.splice(index, 1);
-        $scope.alert = 'Student deleted.';
+    $scope.deleteStudent = function (index) {
+        if (confirm('Are you sure you want to remove ' + $scope.students[index].firstName + ' ' + $scope.students[index].lastName + ' ?')) {
+            $scope.students.splice(index, 1);
+            $scope.alert = 'Student deleted.';
+        }
     }
 });
---
+++ app_$http.js
@@ -0,0 +1,78 @@
+var app = angular.module('myApp', ['ui.bootstrap.showErrors']);
+app.controller('produitsController', function ($scope, $http) {
+    $scope.produit = {}; //utile pour copyProduct
+
+    // Afficher la liste des produits
+    $http.get('http://carla.naxidia.com:8083/api/produits')
+        .then(function (response) {
+            $scope.produits = response.data;
+        }, function (response) {
+            $scope.produits = response.statusText;
+        });
+
+    // Ajouter un produit
+    $scope.addProduct = function (valid) {
+        $scope.$broadcast('show-errors-check-validity');
+        if (valid) {
+            if ($scope.produit.id == null) {
+                $http.post('http://carla.naxidia.com:8083/api/produits', $scope.produit)
+                    .then(function (response) {
+                        $scope.addSuccess = 'Produit ajouté';
+                        $scope.$broadcast('show-errors-reset');
+                        //Ajouter l'id de $scope.produit à partir de la BDD
+                        $scope.produit.id = response.data.id;
+                        // Ajouter le produit en live (sans actualiser la page)
+                        $scope.produits.unshift($scope.produit);
+
+                        //$scope.form1 = {}; //ne peut pas ajouter un autre produit sans actualiser la page
+
+                        // Vider le formulaire après l'ajout pour qu'on puisse ajouter d'autres produits
+                        $scope.produit = {};
+                    }, (function (response) {
+                            // En cas d'erreur lors de l'ajout, l'erreur s'affiche
+                            $scope.addError = response.status + ' : ' + response.statusText;
+                        }
+                    ))
+            } else {
+                // Ce bout de code est pour la modification (id!=null)
+                $http.put('http://carla.naxidia.com:8083/api/produits/', $scope.produit)
+                    .then(
+                        function (response) {
+                            $scope.modifySuccess = "Produit modifié.";
+                            // Modifier le produit en live (sans actualiser la page)
+                            var index = _.findIndex($scope.produits, {'id': $scope.produit.id});
+                            angular.copy($scope.produit, $scope.produits[index]);
+                        },
+                        function (response) {
+                            // En cas d'erreur lors de l'ajout, l'erreur s'affiche
+                            $scope.modifyError = response.status + ' : ' + response.statusText;
+                        }
+                    );
+            }
+        }
+    }
+
+    // Supprimer un produit
+    $scope.deleteProduct = function (id, index) {
+        if (confirm('Ëtes-vous sûr de vouloir supprimer ' + $scope.produits[index].libelle + '?')) {
+            $http.delete('http://carla.naxidia.com:8083/api/produits/' + id)
+                .then(
+                    function (response) {
+                        $scope.alert = 'Produit ' + $scope.produits[index].libelle + ' supprimé';
+                        // Supprimer le produit en live (sans actualiser la page)
+                        $scope.produits.splice(index, 1);
+                    },
+                    function (response) {
+                        // En cas d'erreur lors de la suppression, l'erreur s'affiche
+                        $scope.error = response.status + ' : ' + response.statusText;
+                    }
+                );
+        }
+    }
+
+    // Copier les données d'un produit dans le formulaire de modification
+    $scope.copyProduct = function (index) {
+        angular.copy($scope.produits[index], $scope.produit);
+    };
+
+});
